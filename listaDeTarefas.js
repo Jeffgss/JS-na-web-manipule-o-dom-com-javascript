92d70cfb753c35344723b37ce5a24ab69fc08ff0
Jefferson Santos <jefferson.gsantos.dev@gmail.com>
2021-06-04 14:18:43 -0300

    03-adding task

--- listaDeTarefas.js
+++ listaDeTarefas.js
@@ -1,8 +1,15 @@
 const criarTarefa = (event) => {
   event.preventDefault();
+
   const input = document.querySelector("[data-form-input]");
-  console.log("funcionando");
-  console.log(input.value);
+  const valor = input.value;
+
+  const tarefa = document.querySelector("[data-task]");
+  const conteudo = `<p class="content">${valor}</p>`;
+
+  tarefa.innerHTML = conteudo;
+
+  input.value = " ";
 };
 
 const novaTarefa = document.querySelector("[data-form-button]");
