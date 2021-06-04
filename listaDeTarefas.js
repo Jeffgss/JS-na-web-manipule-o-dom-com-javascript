ccf11ce75382f05f305d0a432e221b11c14c3d39
Jeff Santos <jefferson.santos@maxmilhas.com.br>
2021-06-04 12:34:56 -0300

    02-capturing input value

--- listaDeTarefas.js
+++ listaDeTarefas.js
@@ -1,5 +1,8 @@
 const novaTarefa = document.querySelector("[data-form-button]");
+const input = document.querySelector("[data-form-input]");
 
-novaTarefa.addEventListener("click", () => {
+novaTarefa.addEventListener("click", (event) => {
   console.log("funcionando");
+  event.preventDefault();
+  console.log(input.value);
 });
