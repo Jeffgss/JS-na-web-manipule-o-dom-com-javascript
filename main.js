1e485996ef067e8eba3518ce55adaf26ce15ff38
Jefferson Santos <jefferson.gsantos.dev@gmail.com>
2021-06-07 09:22:00 -0300

    05-creating modules

---
+++ main.js
@@ -0,0 +1,25 @@
+import BotaConclui from "./components/concluiTarefa.js";
+import BotaoDeleta from "./components/deletaTarefa.js";
+
+const criarTarefa = (event) => {
+  event.preventDefault();
+
+  const lista = document.querySelector("[data-list]");
+  const input = document.querySelector("[data-form-input]");
+  const valor = input.value;
+  const tarefa = document.createElement("li");
+
+  tarefa.classList.add("task");
+  const conteudo = `<p class="content">${valor}</p>`;
+  tarefa.innerHTML = conteudo;
+
+  tarefa.appendChild(BotaConclui());
+  tarefa.appendChild(BotaoDeleta());
+  lista.appendChild(tarefa);
+
+  input.value = "";
+};
+
+const novaTarefa = document.querySelector("[data-form-button]");
+
+novaTarefa.addEventListener("click", criarTarefa);
